714f12b002808516a8894aec772bf41bbde5afd8
John Nkp <john.nkp1@gmail.com>
2024-06-17 23:27:34 +1000

    db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  
+  config.hosts << "https://workflow-rails-api-production.up.railway.app/"
+
 end
