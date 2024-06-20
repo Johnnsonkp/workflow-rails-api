90b8fda72a0693c82d406129da478c75a0ad3272
John Nkp <john.nkp1@gmail.com>
2024-06-21 05:44:49 +1000

    updated settings

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,31 +5,35 @@ class TasksController < ApplicationController
 
 
     def index 
-        tasks = @current_user.tasks
-        # tasks = Task.all
+        tasks = set_current_user.tasks
 
-        if tasks
-            render json: tasks
-            puts "///////////////// Rails environment: #{Rails.env} /////////////////"
-            puts "///////////////// current_user.id: #{@current_user.id} /////////////////"
-            puts "///////////////// user: #{@user} /////////////////"
-            puts "///////////////// user_id: #{user_id} /////////////////"
-            puts "///////////////// token: #{token} /////////////////"
+        header_token = request.headers["Authorization"]
+        token_decoded = cx_decoded_token(header_token)
+        new_user_id = token_decoded.first["user_id"]
+        user_request ||= User.find_by(id: new_user_id)
+
+        user_tasks = user_request.tasks
+
+        if user_request
+
+            puts "///////////////// user_request: #{user_request} /////////////////"
+        end 
+
+        if user_tasks
+            render json: user_tasks
         else
-            render json: {error: "Task could not be found."}
+            # render json: {error: "Task could not be found."}
+            render json: Task.all
         end
-
     end 
 
     def create 
-        # @task = current_user.tasks.build(task_params)
-        # task = set_current_user.Task.new(task_params) 
-        task = @current_user.Task.new(task_params) 
+        task = Task.new(task_params) 
 
         if task.save
             render json: task
         else
-            render json: {error: "Task could not be created. Please try again."}
+            # render json: {error: "Task could not be created. Please try again."}
         end
 
     end 
@@ -76,6 +80,35 @@ class TasksController < ApplicationController
         # params.permit(:request)
         @current_user ||= User.find_by(id: user_id)
     end 
+
+    def authorized_user 
+        params.permit(:Authorization)
+    end
+
+
+    def jwt_key
+        Rails.application.credentials.jwt_key
+    end
+
+    def cx_decoded_token(header_token)
+        begin
+            JWT.decode(header_token, jwt_key, true, { :algorithm => 'HS256' })
+        rescue => exception
+            [{error: "Invalid Token"}]
+        end    
+    end
+
+    # def token
+    #     request.headers["Authorization"]
+    # end
+
+    def user_id
+        decoded_token.first["user_id"]
+    end
+
+    def current_user
+        @user ||= User.find_by(id: user_id)
+    end
 
 end
 
